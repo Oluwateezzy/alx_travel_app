c6d0d12fcfe1f63c60cd092647343e3aa6d3c25a
Oluwatobiloba <oluwateezzy03@gmail.com>
2025-05-19 04:08:42 +0100

    Refactor project structure by removing unused files and adding ASGI, WSGI, and settings configurations; implement API root view and update URL patterns with API documentation.

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'message': 'Welcome to ALX Travel App API',
+    })
